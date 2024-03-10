304f3a0962543c3962a75d984de03b964f245c1b
ahmedgamal115 <ahmedgamal1153gmail.com>
2024-03-10 13:44:19 +0200

    edit style

--- src/Compnents/DisplayProducts.jsx
+++ src/Compnents/DisplayProducts.jsx
@@ -7,7 +7,7 @@ export default function DisplayProducts({ loading, error, data }) {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
-        
+          <div className="w-full grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {loading && <p>Loading...</p>}
           {error && <p>Error{console.error(error)}</p>}
           {
@@ -15,7 +15,7 @@ export default function DisplayProducts({ loading, error, data }) {
               data.length !== 0 ?
                 data.map((product,idx) => (
                   idx < 4 ?
-                  <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                  
                     <Link to={`/order/${product.id}`} key={idx} className="group relative overflow-hidden">
                       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-300 xl:aspect-h-8 xl:aspect-w-7">
                         <img
@@ -29,7 +29,7 @@ export default function DisplayProducts({ loading, error, data }) {
                       <h3 className="mt-4 text-sm text-gray-800">{product.productSize.width} X {product.productSize.height} CM</h3>
                       <p className="mt-1 text-lg font-medium text-gray-700">{product.price} EGP</p>
                     </Link>
-                  </div>
+                  
                   :''
                 ))
               :
@@ -38,6 +38,7 @@ export default function DisplayProducts({ loading, error, data }) {
                   <p className='font-bold text-4xl w-full flex justify-center items-center h-[400px]'> Designs not avaliable</p>
                 </>            
               }
+          </div>
         </div>
     </div>
   );
